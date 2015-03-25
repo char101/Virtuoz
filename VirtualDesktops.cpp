a65e549c43a5d9f0be208c66dbf4937a576d6901
RaMMicHaeL <mmm.maltsev@gmail.com>
2015-03-25 22:38:04 +0200

    Smarter window animation delay

--- VirtualDesktops.cpp
+++ VirtualDesktops.cpp
@@ -50,6 +50,11 @@ public:
 		return true;
 	}
 
+	bool IsSuppressed()
+	{
+		return m_suppressed;
+	}
+
 private:
 	bool m_suppressed = false;
 	ANIMATIONINFO m_settings;
@@ -58,7 +63,7 @@ private:
 namespace
 {
 	bool IsWindowVisibleOnScreen(HWND hWnd);
-	bool ShowWindowOnSwitch(HWND hWnd, bool show, bool activate = false);
+	bool ShowWindowOnSwitch(HWND hWnd, bool show, bool waitForCompletion, bool activate = false);
 }
 
 struct MonitorInfo
@@ -93,7 +98,7 @@ VirtualDesktops::~VirtualDesktops()
 			suppressor.Suppress();
 
 			CWindow window(*i);
-			ShowWindowOnSwitch(window, true);
+			ShowWindowOnSwitch(window, true, suppressor.IsSuppressed());
 		}
 	}
 }
@@ -103,13 +108,20 @@ void VirtualDesktops::SwitchDesktop(int desktopId)
 	if(desktopId == m_currentDesktopId)
 		return;
 
-	WindowsAnimationSuppressor suppressor;
+	WindowsAnimationSuppressor suppressor(false);
+
+	struct CALLBACK_PARAM {
+		VirtualDesktops *pThis;
+		WindowsAnimationSuppressor *pSuppressor;
+	} param = { this, &suppressor };
 
 	m_desktops[m_currentDesktopId].hForegroundWindow = GetForegroundWindow();
 
 	EnumWindows([](HWND hWnd, LPARAM lParam) {
 		CWindow window(hWnd);
-		VirtualDesktops *pThis = reinterpret_cast<VirtualDesktops *>(lParam);
+		CALLBACK_PARAM *pParam = reinterpret_cast<CALLBACK_PARAM *>(lParam);
+		VirtualDesktops *pThis = pParam->pThis;
+		WindowsAnimationSuppressor *pSuppressor = pParam->pSuppressor;
 
 		DesktopInfo &currentDesktop = pThis->m_desktops[pThis->m_currentDesktopId];
 
@@ -118,21 +130,28 @@ void VirtualDesktops::SwitchDesktop(int desktopId)
 			DWORD dwExStyle = window.GetExStyle();
 			if((dwExStyle & WS_EX_APPWINDOW) || !(dwExStyle & WS_EX_TOOLWINDOW)/* || IsWindowVisibleOnScreen(window)*/)
 			{
+				// Suppress animation lazily.
+				pSuppressor->Suppress();
+
 				currentDesktop.zOrderedWindows.push_back(hWnd);
-				ShowWindowOnSwitch(hWnd, false);
+				ShowWindowOnSwitch(hWnd, false, pSuppressor->IsSuppressed());
 			}
 		}
 
 		return TRUE;
-	}, reinterpret_cast<LPARAM>(this));
+	}, reinterpret_cast<LPARAM>(&param));
 
 	m_currentDesktopId = desktopId;
 
 	auto &windows = m_desktops[m_currentDesktopId].zOrderedWindows;
 	for(auto i = windows.rbegin(); i != windows.rend(); ++i)
 	{
+		// Suppress animation lazily.
+		suppressor.Suppress();
+
 		CWindow window(*i);
-		ShowWindowOnSwitch(window, true, window == m_desktops[m_currentDesktopId].hForegroundWindow);
+		ShowWindowOnSwitch(window, true, suppressor.IsSuppressed(), 
+			window == m_desktops[m_currentDesktopId].hForegroundWindow);
 	}
 
 	windows.clear();
@@ -175,7 +194,7 @@ namespace
 		return param.isVisible;
 	}
 
-	bool ShowWindowOnSwitch(HWND hWnd, bool show, bool activate /*= false*/)
+	bool ShowWindowOnSwitch(HWND hWnd, bool show, bool waitForCompletion, bool activate /*= false*/)
 	{
 		assert(show || !activate); // can't hide and activate
 
@@ -183,6 +202,12 @@ namespace
 			(activate ? 0 : SWP_NOACTIVATE | SWP_NOZORDER) |
 			(show ? SWP_SHOWWINDOW : SWP_HIDEWINDOW);
 		bool result = FALSE != SetWindowPos(hWnd, activate ? HWND_TOP : NULL, 0, 0, 0, 0, dwSWPflags);
+
+		if(waitForCompletion)
+		{
+			// Wait for the window to actually show/hide, with a timeout of 200 ms
+			SendMessageTimeout(hWnd, WM_NULL, 0, 0, SMTO_ABORTIFHUNG, 200, NULL);
+		}
 
 		return result;
 	}
